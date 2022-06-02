32e762af78b62f27d64e13f8e03bb4d6802cf769
Jessica Britton <jessica.britton@sagebase.org>
2022-06-02 10:30:41 -0700

    Add support for validating the new protoemic_distribution_data.json file

--- src/rules/rules_gene_info_nominated_target.R
+++ src/rules/rules_gene_info_nominated_target.R
@@ -2,6 +2,8 @@ rules_gene_info_nominated_target <-  validator(
   
   # data_synapseid - is this used?
   is.vector(data_synapseid),
+  field_length(data_synapseid, min=3, max=25),
+  !is.na(data_synapseid), # checking
   
   # data_used_to_support_target_selection
   is.character(data_used_to_support_target_selection),
@@ -21,31 +23,43 @@ rules_gene_info_nominated_target <-  validator(
   # initial_nomination
   is.numeric(initial_nomination),
   initial_nomination >= 2018,
+  !is.na(initial_nomination), 
   
   # input_data
   is.character(input_data),
+  field_length(input_data, min=2, max=200),
+  !is.na(input_data), 
   
   # predicted_therapeutic_direction
   is.character(predicted_therapeutic_direction),
+  field_length(predicted_therapeutic_direction, min=2, max=200),
+  !is.na(predicted_therapeutic_direction), # checking 
   
-  # rank - is this used?
+  # rank - not used?
   is.character(rank), 
+  !is.na(rank), 
   
   # source
   source %in% c("Community", "AMP-AD", "Resilience-AD", "TREAT-AD"),
+  !is.na(source), 
   
   # study
   is.character(study),
+  !is.na(study),
+  field_length(study, min=2, max=50),
   
   # target_choice_justification
   is.character(target_choice_justification),
+  !is.na(target_choice_justification), 
+  field_length(target_choice_justification, min=2, max=200),
   
   # team
   is.character(team),
-  field_length(team, min=3, max=50),  
+  field_length(team, min=3, max=50),
   
   # validation_study_details
   is.character(validation_study_details),
-  field_length(validation_study_details, min=0, max=1000)
+  field_length(validation_study_details, min=0, max=1000),
+  !is.na(study)
   
 )
---
+++ src/rules/rules_proteomics_distribution_data.R
@@ -0,0 +1,32 @@
+rules_proteomics_distribution_data <- validator(
+  
+  # type
+  is.character(type),
+  field_length(type, min=3, max=3), 
+  type == 'LFQ',
+  
+  # tissue
+  is.character(tissue),
+  tissue %in% c("AntPFC", "DLPFC", "MFG", "TCX"),
+  
+  # min
+  is.numeric(min),
+  
+  # max
+  is.numeric(max),
+  
+  # median
+  is.numeric(median),
+  
+  # first_quartile
+  is.numeric(first_quartile),
+  
+  # third_quartile
+  is.numeric(third_quartile),
+  
+  # multi-field validation
+  max > third_quartile,
+  third_quartile > median,
+  median > first_quartile,
+  first_quartile > min
+)
